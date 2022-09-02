762cfbc04503a40d26c0c63ce7b9dde217264cb7
i-serra <104382671+i-serra@users.noreply.github.com>
2022-09-02 23:44:08 +0100

    Added dictionary search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+        <header>
      hello
+     </header>
+     <main>
+       <Dictionary/>
+     </main>
+     <footer>Coded by Inês Serra</footer>
+     </div>
     </div>
   );
 }
