e97e04fb9ae92e8b6177ea5f5331c92e539428e0
i-serra <104382671+i-serra@users.noreply.github.com>
2022-09-11 23:14:03 +0100

    Included the a phonetics component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,6 @@ export default function Phonetic(props){
         <a href={props.phonetic.audio} target="_blank">
             Listen
         </a>
-        <br/>
         {props.phonetic.text}
     </div>
 }
