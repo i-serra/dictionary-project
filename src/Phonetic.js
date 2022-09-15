183052ccd8682e01d4ff2aa7b3f51d02df2fe314
i-serra <104382671+i-serra@users.noreply.github.com>
2022-09-16 00:38:32 +0100

    Updated the styling of the page

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -11,6 +11,6 @@ export default function Phonetic(props){
 
     return <div className="Phonetic">
         <a href="#0" onClick={start}><i className="fa-solid fa-microphone"></i></a>
-        <span>{props.phonetic.text}</span>
+        <span className="phonetic-text">{props.phonetic.text}</span>
     </div>
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,7 @@ export default function Synonyms(props){
         return (
             <ul className="Synonyms">
         {props.synonyms.map(function(synonym, index){
-            return <li key={index}>
+            return <li key={index} className="shadow-sm rounded border">
                 {synonym}
             </li>
         })}
