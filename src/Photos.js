d018d76cc6f0d123b6d859ae6e8b0857c3fffeef
i-serra <104382671+i-serra@users.noreply.github.com>
2022-09-20 00:26:06 +0100

    Added the alt text to an image

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ export default function Photos(props){
                     return (
                             <div className="col-12">
                     <a href={photo.src.original} rel="noreferrer nonopener" target="_blank" key={index}>
-                        <img src={photo.src.tiny} className="shadow rounded"/>
+                        <img src={photo.src.tiny} className="shadow rounded" alt="#"/>
                     </a>
                     </div>
                     
